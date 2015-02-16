bd8ec7bcff2f20643bd18c695847a3d1d498db8d
Derek Warren <warren@sfu.ca>
2015-02-16 11:46:22 -0800

    Initial import

---
+++ README.md
@@ -0,0 +1,62 @@
+# puppet-sfu_mod_auth_cas
+
+Manages the SFU flavour of mod_auth_cas for Apache on CentOS.
+
+
+### Usage
+
+```
+class { 'sfu_mod_auth_cas': }
+```
+
+Override the defaults as needed:
+
+```
+class { 'sfu_mod_auth_cas':
+   ca_cert_bundle_path => 'TotallyNotVulnerableBundle.pem',
+   cas_cookie_path => '/var/www/cascookies',
+}
+```
+
+### Parameters
+
+**cas_apache_module** 
+
+One day when this modules doesn't suck, we'll actually check that you've compiled and installed mod_auth_cas.
+
+
+**auth_cas_conf**
+
+Location of auth_cas.conf include file. Default: /etc/httpd/conf.d/auth_cas.conf
+
+
+**auth_cas_load**
+
+Location of auth_cas.load include file. Default: /etc/httpd/load.d/auth_cas.load
+
+
+**ca_cert_bundle_path**
+
+CASCertificatePath for mod_auth_cas. Default: /etc/ssl/certs/ThawtePremiumServerBundleCA.pem
+
+
+**ca_cert_bundle_source**
+
+URL of current ThawtePremiumServerBundleCA.pem.
+
+
+**cas_cookie_path**
+
+CASCookiePath for mod_auth_cas, which insists on a trailing slash here. Default: /var/cascookies/
+
+Other mod_auth_cas parameters you can override:
+cas_login_url
+cas_validate_url
+cas_tickets_url
+cas_allow_wildcard_cert
+cas_validate_server
+
+### Version
+0.1 - 18-Feb-2015 - Initial release
+
+
---
+++ manifests/init.pp
@@ -0,0 +1,64 @@
+class sfu_mod_auth_cas (
+  # one day we'll error out if this isn't present
+  $cas_apache_module       = '/usr/lib64/httpd/modules/mod_auth_cas.so', 
+  $auth_cas_conf           = '/etc/httpd/conf.d/auth_cas.conf',
+  $auth_cas_load           = '/etc/httpd/conf.d/auth_cas.load',
+  $ca_cert_bundle_path     = '/etc/ssl/certs/ThawtePremiumServerBundleCA.pem',
+  $ca_cert_bundle_source   = 'http://www.sfu.ca/content/dam/sfu/itservices/publishing/cas/ThawtePremiumServerBundleCA.pem',
+  $cas_cookie_path         = '/var/cascookies/', # mod_auth_cas wants a trailing slash
+  $cas_login_url           = 'https://cas.sfu.ca/cas/login',
+  $cas_validate_url        = 'https://cas.sfu.ca/cas/serviceValidate',
+  $cas_tickets_url         = 'https://cas.sfu.ca/cas/rest/v1/tickets',
+  $cas_allow_wildcard_cert = 'Off',
+  $cas_validate_server     = 'On',
+  ) {
+
+  case $::operatingsystem {
+    redhat, centos, fedora: {      
+      wget::fetch { $ca_cert_bundle_path:
+        source      => $ca_cert_bundle_source,
+        destination => $ca_cert_bundle_path,
+        timeout     => 30,
+        verbose     => false,
+      }
+      
+      concat { $auth_cas_conf:
+        warn    => true,
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+      }
+
+      concat { $auth_cas_load:
+        warn    => true,
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+      }
+
+      
+      concat::fragment { 'auth_cas.load.base':
+        target  => $auth_cas_load,
+        order   => 10,
+        content => template('mod_auth_cas_sfu/auth_cas.load.erb'),
+      }
+      
+      concat::fragment { 'auth_cas.conf.base':
+        target  => $auth_cas_conf,
+        order   => 05,
+        content => template('mod_auth_cas_sfu/auth_cas.conf.erb'),
+      }
+      
+      file { $cas_cookie_path:
+        ensure  => directory,
+        recurse => true,
+        mode    => '0770',
+        owner   => 'apache',
+        group   => 'apache',
+      }
+    }
+    default: {
+      fail('Unsupported operating system')
+    }
+  }
+}
